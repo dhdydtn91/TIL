50c833def846c292121ef0f0898270a08b5d076d
oyongsoo <dhdydtn2019@gmail.com>
2021-10-12 19:32:54 +0900

    String 관련 알고리즘 문제풀이

---
+++ algorithm_exam/src/main/java/string/PlusOne.java
@@ -0,0 +1,20 @@
+package string;
+
+public class PlusOne {
+    public static int[] solve (int[] k){
+
+        for(int i = k.length -1 ; i > -1  ; i++){
+            k[i]++;
+            if(k[i] < 10){
+                return k;
+            }
+            k[i] =0;
+        }
+        k = new int [k.length+1];
+        k[0] =1;
+
+
+        return k;
+    }
+
+}
