54658688eebfa26938be250517cf4e7dbf9ccd04
oyongsoo <dhdydtn2019@gmail.com>
2021-10-14 00:12:25 +0900

    시큐리티 연습 - 회원가입까지

---
+++ security/src/main/java/com/security/basic/controller/IndexController.java
@@ -0,0 +1,67 @@
+package com.security.basic.controller;
+
+import com.security.basic.model.User;
+import com.security.basic.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"","/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String password = user.getPassword();
+        String encodedPwd = bCryptPasswordEncoder.encode(password);
+        user.setPassword(encodedPwd);
+        userRepository.save(user); //회원가입 잘됨. 비밀번호 : 1234 = >암호가 안되어서 시큐리티로 로그인 할 수 없음
+        return "redirect:/loginForm";
+    }
+}
---
+++ security/src/main/java/com/security/basic/model/User.java
@@ -0,0 +1,28 @@
+package com.security.basic.model;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
