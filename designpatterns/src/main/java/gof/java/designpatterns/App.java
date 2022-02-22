eb497cd868d1b8a079ed55c8e35a5ee6f6c384ac
Oh yong Soo <dhdydtn2019@gmail.com>
2022-02-22 23:57:45 +0900

    인프런 디자인패턴 강의 싱글톤 - 안전하고 단순하게 구현하는 방법 - 1        (enum)

--- designpatterns/src/main/java/gof/java/designpatterns/App.java
+++ designpatterns/src/main/java/gof/java/designpatterns/App.java
@@ -1,20 +1,30 @@
 package gof.java.designpatterns;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class App {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings settings = Settings.getInstance();
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
+        Settings settings = Settings.INSTANCE;
         Settings settings1 = null;
+
+        Constructor<?>[] constructor = Settings.class.getDeclaredConstructors();
+        for (Constructor<?> construct : constructor) {
+            construct.setAccessible(true);
+            settings1 = (Settings) construct.newInstance("INSTANCE");
+        }
+        System.out.println(settings == settings1);
+        Settings settings2 = null;
+
         try (ObjectOutput output = new ObjectOutputStream(new FileOutputStream("settings.obj"))){
             output.writeObject(settings);
         }
 
         try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))){
-            settings1 = (Settings) in.readObject();
+            settings2 = (Settings) in.readObject();
         }
-
-        System.out.println(settings == settings1);
+        System.out.println(settings == settings2);
     }
 }
--- designpatterns/src/main/java/gof/java/designpatterns/Settings.java
+++ designpatterns/src/main/java/gof/java/designpatterns/Settings.java
@@ -2,20 +2,7 @@ package gof.java.designpatterns;
 
 import java.io.Serializable;
 
-public class Settings implements Serializable {
+public enum Settings {
+    INSTANCE;
 
-    private Settings() {}
-
-    private static class SettingsHolder {
-        private static final Settings INSTANCE = new Settings();
-    }
-
-    public static Settings getInstance(){
-        return SettingsHolder.INSTANCE;
-    }
-
-    //역직렬화 직렬화 사용 시 대응 방안
-    protected Object readResolve(){
-        return getInstance();
-    }
 }
