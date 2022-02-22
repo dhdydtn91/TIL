522acca96a6e83198e1e22be1d24091b20929228
Oh yong Soo <dhdydtn2019@gmail.com>
2022-02-22 23:16:50 +0900

    인프런 디자인패턴 강의 싱글톤 - 멀티 스레드 환경에서 안전하게 구현하는 방법 3        (double checked locking )

--- designpatterns/src/main/java/gof/java/designpatterns/Settings.java
+++ designpatterns/src/main/java/gof/java/designpatterns/Settings.java
@@ -2,11 +2,19 @@ package gof.java.designpatterns;
 
 public class Settings {
 
-    private static final Settings INSTANCE = new Settings();
+    private static volatile Settings instance;
 
     private Settings() {}
 
     public static Settings getInstance() {
-        return INSTANCE;
+        if(instance == null){
+            synchronized (Settings.class) {
+                if(instance == null){
+                    instance = new Settings();
+                }
+            }
+        }
+
+        return instance;
     }
 }
