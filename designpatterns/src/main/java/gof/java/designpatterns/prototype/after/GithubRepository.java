4f7fd4df584521956b9b6281ab67b859aa11e6ed
Oh yong Soo <dhdydtn2019@gmail.com>
2022-03-01 22:43:43 +0900

    디자인 패턴 - 프로토타입 예제

---
+++ designpatterns/src/main/java/gof/java/designpatterns/prototype/after/GithubRepository.java
@@ -0,0 +1,24 @@
+package gof.java.designpatterns.prototype.after;
+
+public class GithubRepository {
+
+    private String user;
+
+    private String name;
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ designpatterns/src/main/java/gof/java/designpatterns/prototype/before/GithubIssue.java
@@ -0,0 +1,49 @@
+package gof.java.designpatterns.prototype.before;
+
+import java.util.Objects;
+
+public class GithubIssue implements Cloneable {
+
+    private int id;
+
+    private String title;
+
+    private GithubRepository repository;
+
+    public GithubIssue (GithubRepository repository){
+        this.repository = repository;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public GithubRepository getRepository() {
+        return repository;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setRepository(GithubRepository repository) {
+        this.repository = repository;
+    }
+
+    public String getUrl() {
+        return String.format("https://github.com/%s/%s/issues/%d",
+                repository.getUser(),
+                repository.getName(),
+                this.getId());
+    }
+
+
+}
