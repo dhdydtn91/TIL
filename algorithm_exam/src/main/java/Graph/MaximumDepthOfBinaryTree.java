f234c6ac164c5a45a796e768f511d1e0fb9565b9
Oh yong Soo <dhdydtn2019@gmail.com>
2022-03-03 23:41:25 +0900

    알고리즘 풀이 - graph

---
+++ algorithm_exam/src/main/java/Graph/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,42 @@
+package Graph;
+
+import java.util.Queue;
+
+public class MaximumDepthOfBinaryTree {
+
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(3);
+        tree.left = new TreeNode(1);
+        tree.right = new TreeNode(4);
+
+        tree.left.left = new TreeNode(5);
+        tree.left.right = new TreeNode(8);
+        tree.left.left.right = new TreeNode(7);
+
+        MaximumDepthOfBinaryTree a = new MaximumDepthOfBinaryTree();
+        System.out.println(a.maxDepth(tree));
+
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+
+        return Math.max(leftMax,rightMax)+1;
+
+
+    }
+}
+
+class TreeNode{
+    int val;
+    TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
